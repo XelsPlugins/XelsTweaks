31ca2e132670bfb33f6c9d8c05cf1a2a3e79dfad
Xeltor <Xeltor@users.noreply.github.com>
2026-05-22 13:51:59 +0200

    fix: allow forced fitting room closes

--- XelsTweaks/Tweaks/Interface/KeepTryOnOpenTweak.cs
+++ XelsTweaks/Tweaks/Interface/KeepTryOnOpenTweak.cs
@@ -1,6 +1,7 @@
 using System;
 using Dalamud.Game.Agent;
 using Dalamud.Game.Agent.AgentArgTypes;
+using Dalamud.Game.ClientState.Conditions;
 
 namespace XelsTweaks.Tweaks.Interface;
 
@@ -9,6 +10,17 @@ internal sealed class KeepTryOnOpenTweak : TweakBase
     public const string TweakId = "interface.keepTryOnOpen";
 
     private static readonly TimeSpan SuppressionWindow = TimeSpan.FromMilliseconds(750);
+    private static readonly ConditionFlag[] ForcedTryOnCloseConditionFlags =
+    [
+        ConditionFlag.InCombat,
+        ConditionFlag.BetweenAreas,
+        ConditionFlag.BetweenAreas51,
+        ConditionFlag.WatchingCutscene,
+        ConditionFlag.WatchingCutscene78,
+        ConditionFlag.OccupiedInEvent,
+        ConditionFlag.OccupiedInQuestEvent,
+        ConditionFlag.Occupied39,
+    ];
 
     private DateTimeOffset suppressTryonHideUntil = DateTimeOffset.MinValue;
     private bool listenersRegistered;
@@ -27,6 +39,7 @@ internal sealed class KeepTryOnOpenTweak : TweakBase
     {
         this.Services.AgentLifecycle.RegisterListener(AgentEvent.PreHide, AgentId.Inspect, this.OnInspectPreHide);
         this.Services.AgentLifecycle.RegisterListener(AgentEvent.PreHide, AgentId.Tryon, this.OnTryonPreHide);
+        this.Services.Condition.ConditionChange += this.OnConditionChanged;
         this.listenersRegistered = true;
     }
 
@@ -37,6 +50,7 @@ internal sealed class KeepTryOnOpenTweak : TweakBase
             return;
         }
 
+        this.Services.Condition.ConditionChange -= this.OnConditionChanged;
         this.Services.AgentLifecycle.UnregisterListener(AgentEvent.PreHide, AgentId.Tryon, this.OnTryonPreHide);
         this.Services.AgentLifecycle.UnregisterListener(AgentEvent.PreHide, AgentId.Inspect, this.OnInspectPreHide);
         this.suppressTryonHideUntil = DateTimeOffset.MinValue;
@@ -45,6 +59,12 @@ internal sealed class KeepTryOnOpenTweak : TweakBase
 
     private void OnInspectPreHide(AgentEvent eventType, AgentArgs args)
     {
+        if (this.IsForcedTryOnCloseConditionActive())
+        {
+            this.suppressTryonHideUntil = DateTimeOffset.MinValue;
+            return;
+        }
+
         this.suppressTryonHideUntil = DateTimeOffset.UtcNow + SuppressionWindow;
     }
 
@@ -55,8 +75,48 @@ internal sealed class KeepTryOnOpenTweak : TweakBase
             return;
         }
 
+        if (this.IsForcedTryOnCloseConditionActive())
+        {
+            this.suppressTryonHideUntil = DateTimeOffset.MinValue;
+            return;
+        }
+
         args.PreventOriginal();
         this.suppressTryonHideUntil = DateTimeOffset.MinValue;
         this.Services.Log.Debug("Suppressed Fitting Room hide requested while Character Inspect was closing.");
+    }
+
+    private void OnConditionChanged(ConditionFlag flag, bool value)
+    {
+        if (value && IsForcedTryOnCloseConditionFlag(flag))
+        {
+            this.suppressTryonHideUntil = DateTimeOffset.MinValue;
+        }
+    }
+
+    private bool IsForcedTryOnCloseConditionActive()
+    {
+        foreach (var flag in ForcedTryOnCloseConditionFlags)
+        {
+            if (this.Services.Condition[flag])
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private static bool IsForcedTryOnCloseConditionFlag(ConditionFlag flag)
+    {
+        foreach (var forcedCloseFlag in ForcedTryOnCloseConditionFlags)
+        {
+            if (forcedCloseFlag == flag)
+            {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
